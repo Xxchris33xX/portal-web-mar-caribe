a8a763d3cbca29b257ebfbc3fc71ac62dc479b4b
Xxchris33xX <Xxchris33xX@gmail.com>
2022-04-12 00:41:52 -0400

    MVC arreglado

--- mvcPortalWeb/App/Controllers/Sistema/con-proController.php
+++ mvcPortalWeb/App/Controllers/Sistema/con-proController.php
@@ -1,11 +1,7 @@
 <?php
     require('../../Models/ProductModel.php');
     $pro = new productModel();
-    
-        $datos = $pro ->getProduct();
-        if(isset ($_POST["borrar"]) && $_POST["borrar"] == "true"){
-          $pro -> delete_producto($_GET["id"]);
-          exit;
-      }
+    $datos = $pro ->getProduct();
+      
     require('../../Views/Sistema/con-pro.php');
 ?>
